29ecf115d7717f51b87f788c32f122fd10d5768c
SteveImmanuel <steveandreasimanuel@gmail.com>
2026-06-03 12:34:20 +0000

    Add configure node

--- nodes/lance.py
+++ nodes/lance.py
@@ -1,32 +1,46 @@
 import folder_paths
 import comfy.utils
 import comfy.model_management as mm
-
+import os.path as osp
 from transformers.models.qwen2_5_vl.configuration_qwen2_5_vl import Qwen2_5_VLVisionConfig
 from ..modeling.lance.lance import Lance, LanceConfig
-from ..config.config_factory import InferenceArguments
+from ..config.config_factory import InferenceArguments, ModelArguments
 from ..common.utils.misc import AutoEncoderParams
 
+from ..modeling.lance.qwen2_navit import Qwen2ForCausalLM
+from comfy.text_encoders.qwen_vl import Qwen2VLVisionTransformer
+from ..data.data_utils import add_special_tokens
 
 class LanceLoader:
     CATEGORY = "Lance"
-    RETURN_TYPES = ("LANCE_MODEL",)
+    RETURN_TYPES = ("LANCE",)
     FUNCTION = "load"
 
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
+                "model_args": ("MODEL_ARGS",),
+                "inference_args": ("INFERENCE_ARGS",),
+                "vae_config": ("VAE_CONFIG",),
                 "qwen2_causal_lm": ("Qwen2CausalLM",),
-                "vae": ("VAE",),
-                "ckpt_path": (folder_paths.get_filename_list("diffusion_models"),),
             },
             "optional": {
                 "vit": ("VIT",),
+                "vit_config": ("VIT_CONFIG",),
             },
         }
 
-    def load(self, qwen2_causal_lm, vae, ckpt_path, vit=None):
+    def load(
+        self, 
+        model_args: ModelArguments,
+        inference_args: InferenceArguments,
+        vae_config: AutoEncoderParams,
+        qwen2_causal_lm: Qwen2ForCausalLM, 
+        vit: Qwen2VLVisionTransformer=None,
+        vit_config: Qwen2_5_VLVisionConfig=None,
+    ):
+        ckpt_path = osp.join(model_args.model_path, "model.safetensors")
         ckpt_path = folder_paths.get_full_path_or_raise("diffusion_models", ckpt_path)
 
         llm_patcher = qwen2_causal_lm
@@ -36,40 +50,31 @@ class LanceLoader:
         vit_model = vit_patcher.model if vit_patcher is not None else None
         llm_config = language_model.config
 
-        # Lance-level config (mirrors inference_lance.py:487-500). z_channels/downsample
-        # describe the Wan2.2 VAE; latent_patch_size=(1,1,1) gives the shipped patch_latent_dim=48.
-        vae_config = AutoEncoderParams(z_channels=48, downsample_spatial=16, downsample_temporal=4)
-        vit_config = Qwen2_5_VLVisionConfig.from_json_file(...) if vit_model is not None else None
-        training_args = InferenceArguments(
-            visual_gen=True,
-            visual_und=vit_model is not None,
-            freeze_und=False,
-            apply_qwen_2_5_vl_pos_emb=True,
-            timestep_shift=1.0,
-        )
-
         config = LanceConfig(
-            visual_gen=True,
-            visual_und=vit_model is not None,
+            visual_gen=inference_args.visual_gen,
+            visual_und=inference_args.visual_und,
             llm_config=llm_config,
-            vit_config=vit_config,
-            vae_config=vae_config,
-            latent_patch_size=(1, 1, 1),
-            max_num_frames=121,
-            max_latent_size=64,
-            vit_max_num_patch_per_side=70,
-            connector_act="gelu_pytorch_tanh",
-            interpolate_pos=False,
-            timestep_shift=training_args.timestep_shift,
+            vit_config=vit_config if inference_args.visual_und else None,
+            vae_config=vae_config if inference_args.visual_gen else None,
+            latent_patch_size=model_args.latent_patch_size,
+            max_num_frames=model_args.max_num_frames,
+            max_latent_size=model_args.max_latent_size,
+            vit_max_num_patch_per_side=model_args.vit_max_num_patch_per_side,
+            connector_act=model_args.connector_act,
+            interpolate_pos=model_args.interpolate_pos,
+            timestep_shift=inference_args.timestep_shift,
         )
 
         lance = Lance(
             language_model=language_model,
             vit_model=vit_model,
-            vit_type="qwen_2_5_vl_original",
+            vit_type=model_args.vit_type,
             config=config,
-            training_args=training_args,
+            training_args=inference_args,
         )
+
+        if inference_args.copy_init_moe:
+            lance.language_model.init_moe()
 
         ckpt = comfy.utils.load_torch_file(ckpt_path)
         glue_sd = {
@@ -79,9 +84,49 @@ class LanceLoader:
         }
         missing, unexpected = lance.load_state_dict(glue_sd, strict=False)
 
-        return ({
-            "lance": lance,
-            "llm_patcher": llm_patcher,
-            "vit_patcher": vit_patcher,
-            "vae": vae,
-        },)
+        return (lance,)
+
+
+class LanceConfigure:
+    CATEGORY = "Lance"
+    RETURN_TYPES = ("LANCE", "TOKENIZER", "NEW_TOKEN_IDS")
+    FUNCTION = "configure"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "model_args": ("MODEL_ARGS",),
+                "lance": ("LANCE",),
+                "tokenizer": ("TOKENIZER",),
+            },
+        }
+
+    def configure(
+        self,
+        model_args: ModelArguments,
+        lance: Lance,
+        tokenizer,
+    ):
+        tokenizer, new_token_ids, num_new_tokens = add_special_tokens(tokenizer)
+
+        if num_new_tokens > 0:
+            lance.language_model.resize_token_embeddings(len(tokenizer))
+            lance.config.llm_config.vocab_size = len(tokenizer)
+            lance.language_model.config.vocab_size = len(tokenizer)
+
+        image_token_id = lance.language_model.config.video_token_id
+        new_token_ids.update({"image_token_id": image_token_id})
+        lance.update_tokenizer(tokenizer=tokenizer)
+
+        if model_args.tie_word_embeddings: 
+            lance.language_model.untie_lm_head()
+            lance.language_model.copy_new_token_rows_to_lm_head(num_new_tokens)
+
+            model_args.tie_word_embeddings = False
+            lance.config.llm_config.tie_word_embeddings = False
+        else:
+            assert lance.language_model.get_input_embeddings().weight.data.data_ptr() != lance.language_model.get_output_embeddings().weight.data.data_ptr(), 'tie_word_embeddings conflict'
+
+        lance.eval()
+        return (lance, tokenizer, new_token_ids)
